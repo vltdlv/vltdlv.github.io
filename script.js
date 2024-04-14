185931794954965f537a1b72959a3a7b1dbce672
valentina <valentinadelviscio@hotmail.it>
2024-04-14 15:45:11 +0200

    b

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var imageLink = document.querySelector(".image-link");
-  var img = imageLink.querySelector(".boxes");
-  var originalSrc = img.getAttribute("src"); // Memorizza l'attributo src originale
+  // Seleziona tutti gli elementi con la classe .image-link
+  var imageLinks = document.querySelectorAll(".image-link");
 
-  imageLink.addEventListener("mouseover", function () {
-    var newSrc = img.getAttribute("data-hover-src");
-    img.setAttribute("src", newSrc);
-  });
+  // Itera su ogni elemento .image-link
+  imageLinks.forEach(function (imageLink) {
+    // Seleziona l'immagine all'interno di ciascun .image-link
+    var img = imageLink.querySelector(".boxes");
+    var originalSrc = img.getAttribute("src");
 
-  imageLink.addEventListener("mouseout", function () {
-    img.setAttribute("src", originalSrc); // Ripristina l'immagine originale
+    // Aggiungi l'evento mouseover per cambiare l'immagine al passaggio del mouse
+    imageLink.addEventListener("mouseover", function () {
+      var newSrc = img.getAttribute("data-hover-src");
+      img.setAttribute("src", newSrc);
+    });
+
+    // Aggiungi l'evento mouseout per ripristinare l'immagine originale
+    imageLink.addEventListener("mouseout", function () {
+      img.setAttribute("src", originalSrc);
+    });
   });
 });
